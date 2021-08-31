ae7a6adb82db9fcca649b8664db6d8fecf3752ee
baronnycastro <baronnycastro@gmail.com>
2021-08-31 13:43:52 -0300

    subir exemplos de aula

---
+++ exemplos/Exemplo01.java
@@ -0,0 +1,18 @@
+package exemplos;
+/*
+  Comentário de bloco
+  Exemplo01
+*/
+public class Exemplo01 {
+   
+    public static void main(String[] args){
+        // System.out = saida do sistema(tela)
+        // println = exibir mensagem e pula pra proxima linha(ln)
+        // print = exibir mensagem 
+        System.out.print("Meu primeiro programa");
+        System.out.println("outra mensagem");
+        System.out.print("Meu \nprimeiro \nprograma");
+
+    }
+    
+}
---
+++ exercicios/Exercicio01.java
@@ -0,0 +1,12 @@
+package exercicios; // package = pacote/pasta
+// Atalho? ALT+SHIFT+SETA
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+        System.out.println("Exibir resultados das operações dos números 15 e 17 abaixo");
+        System.out.println("Soma = " + (15+7));
+        System.out.println("Multiplicacao = " + (15*7));
+        System.out.println("Subtração = " + (15-7));
+        System.out.println("Divisão = " + (15.0/7));
+    }
+}
